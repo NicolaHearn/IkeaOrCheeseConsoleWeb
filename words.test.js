fb23f7e0a5b3ca86a9616489a356645b8970ea06
Nicola Hearn <nfhearn@gmail.com>
2023-03-15 08:10:12 +0000

    passes unit test - contains an array of cheese words

--- words.test.js
+++ words.test.js
@@ -5,11 +5,16 @@ describe('words', () => {
         expect(words.ikea).toBeInstanceOf(Array);
     });
 
-    it('contains the word "skogsfraken"', () => {
+    it('has a list of ikea words which includes the word "skogsfraken"', () => {
         expect(words.ikea.includes("skogsfraken")).toEqual(true);
     });
 
     it('contains an array of Cheese words', () => {
         expect(words.cheese).toBeInstanceOf(Array);
-    })
+    });
+
+    it('has a list of cheese words which includes the word "gammalost"', () => {
+        expect(words.cheese.includes("gammalost")).toEqual(true);
+    });
+
 })

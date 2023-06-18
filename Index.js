0202cc21b3d74bbfd88a19137457cb425d52c3f6
Nicola Hearn <nfhearn@gmail.com>
2023-06-18 14:03:34 +0100

    got stuck, will take a look at previous commit

--- Index.js
+++ Index.js
@@ -1,28 +1,29 @@
-const Game = require("./game");
-const prompt = require("prompt-sync")();
+// const Game = require("./game.js");
+
+import { Game } from "./game.js";
+// const prompt = require("prompt-sync")();
 
 function hideGreeting(e) {
   e.preventDefault();
   document.getElementById("playerNamePrompt").style.display = "none";
 }
 
-function greetPlayer(e) {
-  e.preventDefault();
-  const userName = document.getElementById("playerName").value;
-  const playerWelcome = document.getElementById("playerWelcome");
+// function play(e) {
+//   e.preventDefault();
+//   const userName = document.getElementById("playerNameSubmit").value;
+//   const playerWelcome = document.getElementById("playerWelcome");
 
-  playerWelcome.innerHTML = `Hello ${userName}, is the word a type of cheese or an item on sale at Ikea? Type your answer (i or c) after the prompts...`;
-  playerWelcome.style.display = "inline";
-}
+//   playerWelcome.innerHTML = `Hello ${userName}, is the word a type of cheese or an item on sale at Ikea? Type your answer (i or c) after the prompts...`;
+//   playerWelcome.style.display = "inline";
 
-console.log("Let's play Ikea or Cheese");
+//   let game = new Game(userName);
+//   game.playGame();
+// }
 
-let game = new Game(userName);
-game.playGame();
-const playAgain = prompt("Would you like to play again? ");
-if (playAgain === "y") {
-  game = new Game(userName);
-  game.playGame();
-} else {
-  console.log("OK, thanks for playing, see you next time!");
-}
+// const playAgain = prompt("Would you like to play again? ");
+// if (playAgain === "y") {
+//   game = new Game(userName);
+//   game.playGame();
+// } else {
+//   console.log("OK, thanks for playing, see you next time!");
+// }

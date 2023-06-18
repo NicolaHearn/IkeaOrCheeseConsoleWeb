5e61abd9976185eec7e75a4efc69dfd180ae4cb7
Nicola Hearn <nfhearn@gmail.com>
2023-06-18 11:49:12 +0100

    asks the player for their name and greets them

--- Index.js
+++ Index.js
@@ -1,12 +1,21 @@
 const Game = require("./game");
 const prompt = require("prompt-sync")();
 
-function showGreeting() {
-  document.getElementById("playerNamePrompt").style.display = "inline";
+function hideGreeting(e) {
+  e.preventDefault();
+  document.getElementById("playerNamePrompt").style.display = "none";
+}
+
+function greetPlayer(e) {
+  e.preventDefault();
+  const userName = document.getElementById("playerName").value;
+  const playerWelcome = document.getElementById("playerWelcome");
+
+  playerWelcome.innerHTML = `Hello ${userName}, is the word a type of cheese or an item on sale at Ikea? Type your answer (i or c) after the prompts...`;
+  playerWelcome.style.display = "inline";
 }
 
 console.log("Let's play Ikea or Cheese");
-let userName = prompt("Please enter your name to start the game: ");
 
 let game = new Game(userName);
 game.playGame();
